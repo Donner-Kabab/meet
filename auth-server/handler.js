a8ba76477a05f466c5428ac94804efe0b8d88e37
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-06-04 20:55:57 -0600

    updated errors

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -109,7 +109,7 @@ module.exports.getCalendarEvents = async (event) => {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Credentials": true,
         },
-        body: JSON.stringify({events: results.data.items }),
+        body: JSON.stringify({ events: results.data.items }),
       };
     })
     .catch((error) => {
