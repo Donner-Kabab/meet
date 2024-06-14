d218aa6d78a906b18997b3383d13fbef17f16594
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-06-13 21:27:11 -0600

    created more test files, corrected errors and completed testing for scenario 1-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import logo from "./logo.svg";
+import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
--- src/_tests_/app.test.js
+++ src/_tests_/app.test.js
@@ -4,9 +4,19 @@ import { render } from "@testing-library/react";
 import App from "../App";
 
 describe("<App /> component", () => {
+  let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  });
+  
   test("renders list of events", () => {
     const AppDOM = render(<App />).container.firstChild;
     expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
   });
-}); 
+
+  test("render CitySearch", () => {
+    const AppDOM = render().container.firstChild;
+    expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
+  });
+});
 ////////////////////This is the basic structure used to test React components//////////////////////////////////
