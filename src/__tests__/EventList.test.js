342cb09651cf0aae4af2e898be3ee25de341663c
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-06-26 17:53:57 -0600

    fixed errors in eventlist.test

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,6 +1,7 @@
 // src/__tests__/EventList.test.js
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
+import App from "../App";
 import EventList from "../components/EventList";
 import { getEvents } from "../api";
 import NumberOfEvents from "../components/NumberOfEvents";
@@ -23,7 +24,20 @@ describe("<EventList /> component", () => {
   test("renders correct number of events", async () => {
     const allEvents = await getEvents();
     EventListComponent.rerender(<EventList events={allEvents} />);
-    expect(EventListComponent.queryByRole("number-of-events")).toBeInTheDocument();
+    expect(
+      EventListComponent.queryByRole("number-of-events")
+    ).toBeInTheDocument();
   });
-  
-})
+});
+
+describe("<EventList /> integration", () => {
+  test("renders a list of 32 events when the app is mounted and rendered", async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+      expect(EventListItems.length).toBe(0);
+    });
+  });
+});
--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,6 +1,6 @@
 // src/__tests__/App.test.js
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
 import App from "../App";
 
 describe("<App /> component", () => {
@@ -16,3 +16,4 @@ describe("<App /> component", () => {
     expect(component).toBeDefined();
   });
 });
+
